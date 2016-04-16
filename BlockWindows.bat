2e0208b7639a842f082c5fe78ba183d10a2485a7
Unrepentant-Atheist <Proxydent@gmx.de>
2016-04-16 22:25:25 +0200

    Update BlockWindows.bat, refactor code a bit

--- BlockWindows.bat
+++ BlockWindows.bat
@@ -1,101 +1,135 @@
 @echo off
 openfiles.exe 1>nul 2>&1
-if not %errorlevel% equ 0 (
-    Echo You are not administrator! Right Click file select run as admin
-    pause
-    EXIT 1
-) else (   
-
 SETLOCAL
 
 REM --- uninstall updates
-echo uninstalling updates, be patient...
-echo Delete KB971033 (license validation)
-start "title" /b /wait wusa.exe /kb:971033 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB2902907 (Microsoft Security Essentials)
-start "title" /b /wait wusa.exe /kb:2902907 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB2952664 (Get Windows 10 assistant)
-start "title" /b /wait wusa.exe /kb:2952664 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB2976978 (update for windows 8.1 and windows 8)
-start "title" /b /wait wusa.exe /kb:2976978 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB2977759 (update for windows 7 rtm)
-start "title" /b /wait wusa.exe /kb:2977759 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB2990214 (Get Windows 10 for Win7)
-start "title" /b /wait wusa.exe /kb:2990214 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3012973 (Upgrade to Windows 10 Pro)
-start "title" /b /wait wusa.exe /kb:3012973 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3014460 (Upgrade for windows insider preview / upgrade to windows 10)
-start "title" /b /wait wusa.exe /kb:3014460 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3015249 (Upgrade that adds telemetry points to consent.exe in Windows 8.1 and Windows 7)
-start "title" /b /wait wusa.exe /kb:3015249 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3021917 (update to Windows 7 SP1 for performance improvements)
-start "title" /b /wait wusa.exe /kb:3021917 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3022345 (telemetry)
-start "title" /b /wait wusa.exe /kb:3022345 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3035583 (GWX Update installs Get Windows 10 app in Windows 8.1 and Windows 7 SP1)
-start "title" /b /wait wusa.exe /kb:3035583 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3044374 (Get Windows 10 for Win8.1)
-start "title" /b /wait wusa.exe /kb:3044374 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3050265 (update for Windows Update on Win7)
-start "title" /b /wait wusa.exe /kb:3050265 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3050267 (update for windows update client for windows 8.1 june 2015)
-start "title" /b /wait wusa.exe /kb:3050267 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3065987 (update for Windows Update on Win7/Server 2008R2)
-start "title" /b /wait wusa.exe /kb:3065987 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3068708 (telemetry)
-start "title" /b /wait wusa.exe /kb:3068708 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3075249 (telemetry for Win7/8.1)
-start "title" /b /wait wusa.exe /kb:3075249 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3075851 (update for Windows Update on Win7/Server 2008R2)
-start "title" /b /wait wusa.exe /kb:3075851 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3075853 (update for Windows Update on Win8.1/Server 2012R2)
-start "title" /b /wait wusa.exe /kb:3075853 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3080149 (Telemetry)
-start "title" /b /wait wusa.exe /kb:3080149 /uninstall /quiet /norestart
-
-REM --- uninstall my own added updates
-echo Delete KB3046480
-start "title" /b /wait wusa.exe /kb:3046480 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3058168
-start "title" /b /wait wusa.exe /kb:3058168 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3065988
-start "title" /b /wait wusa.exe /kb:3065988 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3083325
-start "title" /b /wait wusa.exe /kb:3083325 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3083324
-start "title" /b /wait wusa.exe /kb:3083324 /uninstall /quiet /norestart
-echo  - done.
-timeout 5
+echo Uninstalling updates, this may take a while...
+echo kb:2454826
+start /b /wait wusa.exe /kb:2454826 /uninstall /quiet /norestart
+echo kb:2505438
+start /b /wait wusa.exe /kb:2505438 /uninstall /quiet /norestart
+echo kb:2882822
+start /b /wait wusa.exe /kb:2882822 /uninstall /quiet /norestart
+echo kb:2922324
+start /b /wait wusa.exe /kb:2922324 /uninstall /quiet /norestart
+echo kb:2976987
+start /b /wait wusa.exe /kb:2976987 /uninstall /quiet /norestart
+echo kb:3042058
+start /b /wait wusa.exe /kb:3042058 /uninstall /quiet /norestart
+echo kb:3068707
+start /b /wait wusa.exe /kb:3068707 /uninstall /quiet /norestart
+echo kb:3074677
+start /b /wait wusa.exe /kb:3074677 /uninstall /quiet /norestart 
+echo kb:3078667
+start /b /wait wusa.exe /kb:3078667 /uninstall /quiet /norestart
+echo kb:3080351
+start /b /wait wusa.exe /kb:3080351 /uninstall /quiet /norestart
+echo kb:3081437
+start /b /wait wusa.exe /kb:3081437 /uninstall /quiet /norestart
+echo kb:3081954
+start /b /wait wusa.exe /kb:3081954 /uninstall /quiet /norestart
+echo kb:3104002
+start /b /wait wusa.exe /kb:3104002 /uninstall /quiet /norestart
+echo kb:3112343
+start /b /wait wusa.exe /kb:3112343 /uninstall /quiet /norestart
+echo kb:3135445
+start /b /wait wusa.exe /kb:3135445 /uninstall /quiet /norestart
+echo kb:3123862
+start /b /wait wusa.exe /kb:3123862 /uninstall /quiet /norestart
+echo kb:3124275
+start /b /wait wusa.exe /kb:3124275 /uninstall /quiet /norestart
+echo kb:3134814
+start /b /wait wusa.exe /kb:3134814 /uninstall /quiet /norestart
+echo kb:3134814
+start /b /wait wusa.exe /kb:3134814 /uninstall /quiet /norestart
+echo kb:3138612
+start /b /wait wusa.exe /kb:3138612 /uninstall /quiet /norestart
+echo kb:3139929
+start /b /wait wusa.exe /kb:3139929 /uninstall /quiet /norestart
+echo kb:3146449
+start /b /wait wusa.exe /kb:3146449 /uninstall /quiet /norestart
+echo kb:3140185
+start /b /wait wusa.exe /kb:3140185 /uninstall /quiet /norestart
+echo kb:3086255
+start /b /wait wusa.exe /kb:3086255 /uninstall /quiet /norestart
+echo kb:3083710
+start /b /wait wusa.exe /kb:3083710 /uninstall /quiet /norestart
+echo kb:3088195
+start /b /wait wusa.exe /kb:3088195 /uninstall /quiet /norestart
+echo kb:3090045
+start /b /wait wusa.exe /kb:3090045 /uninstall /quiet /norestart
+echo kb:3093513
+start /b /wait wusa.exe /kb:3093513 /uninstall /quiet /norestart
+echo kb:3093983
+start /b /wait wusa.exe /kb:3093983 /uninstall /quiet /norestart
+echo kb:3100773
+start /b /wait wusa.exe /kb:3100773 /uninstall /quiet /norestart
+echo kb:3102810
+start /b /wait wusa.exe /kb:3102810 /uninstall /quiet /norestart
+echo kb:2952664
+start /b /wait wusa.exe /kb:2952664 /uninstall /quiet /norestart
+echo kb:2902907
+start /b /wait wusa.exe /kb:2902907 /uninstall /quiet /norestart
+echo kb:971033
+start /b /wait wusa.exe /kb:971033 /uninstall /quiet /norestart
+echo kb:3126593
+start /b /wait wusa.exe /kb:3126593 /uninstall /quiet /norestart
+echo kb:3126587
+start /b /wait wusa.exe /kb:3126587 /uninstall /quiet /norestart
+echo kb:3014460
+start /b /wait wusa.exe /kb:3014460 /uninstall /quiet /norestart
+echo kb:3012973
+start /b /wait wusa.exe /kb:3012973 /uninstall /quiet /norestart
+echo kb:2990214
+start /b /wait wusa.exe /kb:2990214 /uninstall /quiet /norestart
+echo kb:2977759
+start /b /wait wusa.exe /kb:2977759 /uninstall /quiet /norestart 
+echo kb:2976978
+start /b /wait wusa.exe /kb:2976978 /uninstall /quiet /norestart
+echo kb:3068708
+start /b /wait wusa.exe /kb:3068708 /uninstall /quiet /norestart
+echo kb:3065987
+start /b /wait wusa.exe /kb:3065987 /uninstall /quiet /norestart
+echo kb:3050267
+start /b /wait wusa.exe /kb:3050267 /uninstall /quiet /norestart
+echo kb:3050265
+start /b /wait wusa.exe /kb:3050265 /uninstall /quiet /norestart
+echo kb:3044374
+start /b /wait wusa.exe /kb:3044374 /uninstall /quiet /norestart
+echo kb:3035583
+start /b /wait wusa.exe /kb:3035583 /uninstall /quiet /norestart
+echo kb:3022345
+start /b /wait wusa.exe /kb:3022345 /uninstall /quiet /norestart
+echo kb:3021917
+start /b /wait wusa.exe /kb:3021917 /uninstall /quiet /norestart
+echo kb:3015249
+start /b /wait wusa.exe /kb:3015249 /uninstall /quiet /norestart
+echo kb:3083324
+start /b /wait wusa.exe /kb:3083324 /uninstall /quiet /norestart
+echo kb:3083325
+start /b /wait wusa.exe /kb:3083325 /uninstall /quiet /norestart
+echo kb:3065988
+start /b /wait wusa.exe /kb:3065988 /uninstall /quiet /norestart
+echo kb:3058168
+start /b /wait wusa.exe /kb:3058168 /uninstall /quiet /norestart
+echo kb:3046480
+start /b /wait wusa.exe /kb:3046480 /uninstall /quiet /norestart
+echo kb:3080149
+start /b /wait wusa.exe /kb:3080149 /uninstall /quiet /norestart
+echo kb:3075853
+start /b /wait wusa.exe /kb:3075853 /uninstall /quiet /norestart
+echo kb:3075851
+start /b /wait wusa.exe /kb:3075851 /uninstall /quiet /norestart
+echo kb:3075249
+start /b /wait wusa.exe /kb:3075249 /uninstall /quiet /norestart
+echo DONE!
+timeout 3
 
 REM --- Hide updates
 echo Hiding updates, may take a while be patient...
 
-start "title" /b /wait cscript.exe "%~dp0HideWindowsUpdates.vbs" 971033 2902907 2952664 2976978 2977759 2990214 3012973 3014460 3015249 3021917 3022345 3035583 3044374 3050265 3050267 3065987 3068708 3075249 3075851 3075853 3080149 3046480 3058168 3065988 3083325 3083324
-echo  - done.
+start /b /wait cscript.exe "%~dp0HideWindowsUpdates.vbs" 2454826 2454826 2882822 2922324 2976987 3042058 3068707 3074677 3078667 3080351 3081437 3081954 3104002 3112343 3135445 3123862 3124275 3134814 3134814 3138612 3139929 3146449 3140185 3086255 3083710 3088195 3090045 3093513 3093983 3100773 3102810 2952664 2902907 971033 3126593 3126587 3014460 3012973 2990214 2977759 2976978 3068708 3065987 3050267 3050265 3044374 3035583 3022345 3021917 3015249 3083324 3083325 3065988 3058168 3046480 3080149 3075853 3075851 3075249
+echo  DONE!
 
 REM --- Disable tasks
 echo Disabling tasks. Depending on Windows version this may have errors, this is normal...
@@ -144,4 +178,3 @@ sc stop remoteregistry
 echo Done. Manually Reboot for changes to take effect
 REM shutdown -r
 pause
-)
